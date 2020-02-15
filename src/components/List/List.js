a11cefb2a9cc58aa2d8335bf045e7a66428191a3
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-02-15 10:27:41 +0100

    Add props in App, Hero, List and props typechecking

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  /* typechecking on the props for a component with prop-types library*/
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  /* default value of prop if prop not provided */
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero/>
+        <Hero titleText={this.props.title}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
