2a1d474083a0ea9ba2f9d9c982f8b7b959875a7b
natkalia <natalia.banasiewicz@wp.pl>
2020-02-29 09:16:29 +0100

    Refactor Card to show source of task only in SearchResults and not in List

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
+import {withRouter} from 'react-router';
 
 class Card extends React.Component {
-  /* typechecking on the props for a component with prop-types library*/
+
   static propTypes = {
     title: PropTypes.string,
     columnId: PropTypes.string,
-    columns: PropTypes.object,    
+    columns: PropTypes.array, 
+    history: PropTypes.object,   
   }
 
   render() {
-    const {title, columnId, columns} = this.props;
+    const {title, columns, columnId} = this.props;
+    const {pathname} = this.props.history.location;
     
     const filteredColumns = columns.filter(column => 
       column.id == columnId ? column : null);
-
+    
     return (
       <section className={styles.component}>
         <span>{title} </span>
         
-        {filteredColumns.map(column => 
-          columnId == column.id ? 
-            (<span 
-              className={styles.source} 
-              key={column.id}>({column.listId}) 
-            </span>) :
-            null
-        )}
-        
-        <span className={styles.source}> ({columnId}) </span>
+        <span className={styles.source}>
+          {pathname.includes('search') ? (
+
+            filteredColumns.map(column => 
+              column.id == columnId ? 
+                (
+                  <span  
+                    key={column.id}>
+                    ({column.listId}) 
+                    ({column.id})  
+                  </span>) :
+                null
+            )
+            
+          ) : (null)}            
+        </span>
 
       </section>
     );
   }
 }
 
-export default Card;
+export default withRouter(Card);
