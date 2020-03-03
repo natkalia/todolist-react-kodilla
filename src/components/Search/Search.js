1d1e227728c74f49395c0a4a39bfdfbeb020203e
natkalia <natalia.banasiewicz@wp.pl>
2020-03-03 14:26:34 +0100

    Update layout style in Card and Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,6 @@ import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
-import Container from '../Container/Container';
 import {withRouter} from 'react-router';
 
 class Search extends React.Component {
@@ -49,29 +48,27 @@ class Search extends React.Component {
     const {pathname} = this.props.history.location;
     return (
       <div className={styles.component}>
-        <Container>
-          <div className={styles.inner}>
-            <input
-              type='text'
-              placeholder={text}
-              value={value}
-              onChange={event => this.handleChange(event)}
-            />
-            <div className={styles.buttons}>
-              <Button onClick={() => 
-                this.handleOK()}>
-                <Icon name={icon} />
-              </Button>
-            </div>
-            <div className={styles.count}>
-              {pathname.includes('search') ? (
-                <span>
-                  {countVisible == countAll ? '' : `${countVisible} / ${countAll}`}
-                </span>
-              ) : (null)}            
-            </div>
+        <div className={styles.inner}>
+          <input
+            type='text'
+            placeholder={text}
+            value={value}
+            onChange={event => this.handleChange(event)}
+          />
+          <div className={styles.buttons}>
+            <Button onClick={() => 
+              this.handleOK()}>
+              <Icon name={icon} />
+            </Button>
           </div>
-        </Container>
+          <div className={styles.count}>
+            {pathname.includes('search') ? (
+              <span>
+                {countVisible == countAll ? '' : `${countVisible} / ${countAll}`}
+              </span>
+            ) : (null)}            
+          </div>
+        </div>
       </div>
     );
   }
